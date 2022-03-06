a5ee03b4f52796df4087fcbb2204ff7083ab3208
Thanx01 <85063163+Thanx01@users.noreply.github.com>
2022-03-06 17:43:00 +0800

    Initial commit

---
+++ src/第二章作业/StudentDAO/bean/Student.java
@@ -0,0 +1,18 @@
+package 第二章作业.StudentDAO.bean;
+
+public class Student{
+   private long iD;
+   private String name;
+
+   public void setID(long iD){
+      this.iD = iD; }
+
+   public long getID(){
+      return iD;}
+
+   public void setName(String name){
+      this.name = name; }
+
+   public String getName(){
+      return name; }
+}
